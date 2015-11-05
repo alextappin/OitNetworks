d53dfc0b532f1678df7e22166f9ce9d66544e01e
t_tappa <alex.tappin@autodesk.com>
2015-11-05 13:24:02 -0800

    Started repo

---
+++ client.js
@@ -0,0 +1,97 @@
+var net = require('net');
+var put = require('put');
+var ip = require('ip');
+
+var serverInfo = {
+    serviceIp : '192.168.101.210',
+    servicePort : 2605
+};
+var counter = 0;
+var requestObject = {
+    tcpHeader   : 0,
+    messageType : 'REQ',
+    fieldSeparator :'|',
+    msTimeStamp : 0,
+    requestID : '',
+    studentName : 'AlexanderTappin',
+    studentID   : '220-361',
+    responseDelay   : 0,
+    clientIPAddress : ip.address(),
+    clientServicePort   : 0,
+    clientSocketNo  : 0,
+    foreignHostIPAddress    : '',
+    foreignHostServicePort  : 0,
+    studentData : 'First',
+    scenarioNo  : 0,
+    totalLength : 0,
+    valueInTCPHeader    : 0
+};
+var unique = 1;
+
+var writeRequest = function(cli) {
+    requestObject.msTimeStamp = Date.now();
+    requestObject.requestID += unique;
+    requestObject.clientServicePort = cli.address().port;
+    requestObject.foreignHostIPAddress = serverInfo.serviceIp;
+    requestObject.foreignHostServicePort = serverInfo.servicePort;
+    requestObject.scenarioNo = 1;
+    var resp = requestObject.messageType + requestObject.fieldSeparator +
+        requestObject.msTimeStamp + requestObject.fieldSeparator + requestObject.requestID +
+        requestObject.fieldSeparator + requestObject.studentName + requestObject.fieldSeparator +
+        requestObject.studentID + requestObject.fieldSeparator + requestObject.responseDelay +
+        requestObject.fieldSeparator + requestObject.clientIPAddress + requestObject.fieldSeparator +
+        requestObject.clientServicePort + requestObject.fieldSeparator + requestObject.clientSocketNo +
+        requestObject.fieldSeparator + requestObject.foreignHostIPAddress + requestObject.fieldSeparator +
+        requestObject.foreignHostServicePort + requestObject.fieldSeparator + requestObject.studentData +
+        requestObject.fieldSeparator + requestObject.scenarioNo + requestObject.fieldSeparator +
+        requestObject.totalLength + requestObject.fieldSeparator + requestObject.valueInTCPHeader;
+
+    return resp;
+};
+
+var calculateLength = function() {
+    var length = 0;
+    return length;
+};
+
+var receiveResponse = function() {
+    return true;
+};
+
+var convertLengthToBinary = function convertLengthToBinary(len) {
+    return parseInt(len, 2);
+};
+
+var messageCount = 0;
+var recieveCount = 0;
+var client = new net.Socket();
+client.connect(serverInfo.servicePort, serverInfo.serviceIp, function() {
+    console.log('Connected');
+    var buf1 = new Buffer(2);
+    var requestString = writeRequest(client);
+    buf1.writeInt16BE(requestString.length);
+    client.write(buf1);
+    client.write(requestString);
+});
+
+client.on('data', function(data) {
+    counter++;
+    console.log('data');
+    console.log(data);
+    console.log(data.toString());
+    console.log("there was data");
+    if (counter < 100) {
+        var buf1 = new Buffer(2);
+        var requestString = writeRequest(client);
+        buf1.writeInt16BE(requestString.length);
+        client.write(buf1);
+        client.write(requestString);
+    }
+    else {
+        client.end();
+    }
+});
+
+client.on('close', function() {
+    console.log('Connection closed');
+});
